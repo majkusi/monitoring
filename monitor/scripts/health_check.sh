0aeb521e6fed1d0420dbed5bb8ea8ca1b6bc9ee0
majkusi <igor34p@gmail.com>
2026-06-13 15:26:56 +0200

    Added resources thresholds with alerts.logs file to alert about limits excessing

--- monitor/scripts/health_check.sh
+++ monitor/scripts/health_check.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
+
+#THRESHOLDS
+DISK_THRESHOLD=80
+RAM_THRESHOLD=85.00
+CPU_THRESHOLD=90.00
+
 while true; do
+
+    timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+    #RAM
     ram_used=$(free -m | awk 'NR==2{print $3}')
     ram_total=$(free -m | awk 'NR==2{print $2}')
     ram_percentage=$(awk "BEGIN {printf \"%.2f\", ($ram_used/$ram_total)*100}")
+    if awk -v ram_pr="$ram_percentage" -v ram_th="$RAM_THRESHOLD" 'BEGIN{exit ram_pr<ram_th}'; then
+        echo "$timestamp, RAM ALERT: ${ram_percentage}% >= ${RAM_THRESHOLD}%" >> /logs/alerts.log
+    fi
+
+    #DISK
     disk_used=$(df -h / | awk 'NR==2{print $5}')
-    timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+    disk_value=$(echo "$disk_used" | tr -d '%')
+    if [ "$disk_value" -gt "$DISK_THRESHOLD" ]; then
+        echo "$timestamp,DISK ALERT: ${disk_value}% > ${DISK_THRESHOLD}%" >> /logs/alerts.log
+    fi
+    
+    #CPU
     cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
     cpu_percentage=$(awk "BEGIN {printf \"%.2f\", $cpu_usage}")
+    if awk -v cpu_pr="$cpu_percentage" -v cpu_th="$CPU_THRESHOLD" 'BEGIN{exit cpu_pr<cpu_th}'; then 
+        echo "$timestamp,CPU ALERT: ${cpu_percentage}% > ${CPU_THRESHOLD}%" >> /logs/alerts.log
+    fi
+
+    #LOGGING INTO FILE
     echo "$timestamp,$ram_percentage,$ram_used,$ram_total,$disk_used,$cpu_percentage" >> /logs/health.log
     status=$(curl -s -o /dev/null -w "%{http_code}" http://tomcat:8080/)
     echo "$timestamp,$status" >> /logs/service.log
